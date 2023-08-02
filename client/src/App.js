09a614ba0ca4d2ed889b9e646f1c771bb366e30d
Wasiuddin Mansoori <121297231+wasiuddinmansoori1@users.noreply.github.com>
2023-08-02 23:17:21 +0530

    add the entering routes to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import Main from './components/Main';
-import Navbar from './components/Nav';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./components/Main";
+import Navbar from "./components/Nav";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Main />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/home" element={<Main />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
