1329e990c8b013ab5123a6fd7535617f753ff698
Wasiuddin Mansoori <121297231+wasiuddinmansoori1@users.noreply.github.com>
2023-08-24 21:23:33 +0530

    Update addpreference.js

--- server/models/addpreference.js
+++ server/models/addpreference.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose");
-
+const User = require("./user");
 const tutSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Entering Information",
+    required: true,
+  },
   genderPreference: {
     type: String,
+    required: true,
   },
   accommodationType: {
     type: String,
+    required: true,
   },
   preferredAge: {
     type: String,
+    required: true,
   },
   topPreference: {
     type: String,
+    required: true,
   },
 });
 
