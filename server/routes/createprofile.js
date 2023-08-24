8f40e9b0f449bbc7f8b8b412a5ba9bd8b80dfb96
Wasiuddin Mansoori <121297231+wasiuddinmansoori1@users.noreply.github.com>
2023-08-24 21:24:51 +0530

    Update createprofile.js

--- server/routes/createprofile.js
+++ server/routes/createprofile.js
@@ -2,29 +2,27 @@ const create_router = require("express").Router();
 const cors = require("cors");
 create_router.use(cors());
 const bodyParser = require("body-parser");
-const Profile  = require("../models/createprofile");
+const Profile = require("../models/createprofile");
 create_router.use(bodyParser.json());
 create_router.use(cors());
 
-
-
-
-create_router.route("/createprofile").post((req, res) => {
+create_router.route("/createprofile").post(async (req, res) => {
+  const { firstName, photo, lastName, gender, age, about, userId } = req.body;
+  // const userId = req.user ? req.user.id : null;
+  // console.log(req.user);
   console.log(req.body);
-  const { Name, lastName, Gender, age, about } = req.body;
-
-  // console.log(req.body);
   const newProfile = new Profile({
-    Name,
+    user: userId,
+    firstName,
+    photo,
     lastName,
-    Gender,
+    gender,
     age,
     about,
   });
-
-    newProfile
+  newProfile
     .save()
-    .then(() => res.json("Profile Created"))
+    .then((createdProfile) => res.json({ message: "Profile Created", newProfile: createdProfile }))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
